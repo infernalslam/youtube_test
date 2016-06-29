fb5372df9aca2df417bea4c18b6507d6695c739e
TAK <infernal-slam@hotmail.com>
2016-06-29 23:47:14 +0700

    success

--- public/app.js
+++ public/app.js
@@ -1,29 +1,33 @@
 /*global angular*/
-angular.module('todoApp', ['ngYoutubeEmbed'])
+angular.module('todoApp', ['ngYoutubeEmbed', 'youtube-embed'])
   .controller('TodoListController', function ($scope, $http, $sce) {
     $scope.title = 'Youtube API'
-    // $scope.data = null
-    // $scope.dataVideo = []
-    // $scope.count = 0
-    // $scope.link = []
-    // $scope.link.push('https://www.youtube.com/watch?v=UNaYpBpRJOY')
-    // $scope.link = 'https://www.youtube.com/watch?v=UNaYpBpRJOY'
-    // $scope.src = $sce.trustAsResourceUrl('http://www.youtube.com/embed/UNaYpBpRJOY?autoplay=1')
+    $scope.video = []
+    $scope.dataVideo = []
+    $scope.video.push('UNaYpBpRJOY')
+    $scope.playerVars = {
+      controls: 1,
+      autoplay: 1
+    }
     $scope.search = function (keyword) {
       $http.get('/search?keyword=' + keyword).then(function (res) {
         console.log(res.data)
         $scope.data = res.data
       })
-      $scope.addVideo = function (id) {
-        // $scope.src = 'http://www.youtube.com/embed/' + id + '?autoplay=1'
-        // $scope.src = $sce.trustAsResourceUrl('http://www.youtube.com/embed/' + id + '?autoplay=1')
-        // $scope.link = 'https://www.youtube.com/watch?v=' + id
-        // $scope.link.push('https://www.youtube.com/watch?v=' + id)
-        // var data = {
-        //   id: 'https://www.youtube.com/watch?v=' + id
-        // }
-        // $scope.dataVideo.push(data)
-        // console.log($scope.dataVideo)
+      $scope.addVideo = function (id, title, pic) {
+        var data = {
+          id: id,
+          title: title,
+          piuc: pic
+        }
+        $scope.dataVideo.push(data)
+        $scope.video.push(id)
+        console.log($scope.video)
       }
+      $scope.$on('youtube.player.ended', function ($event, player) {
+        $scope.dataVideo.shift()
+        $scope.video.shift()
+        player.playVideo()
+      })
     }
   })
